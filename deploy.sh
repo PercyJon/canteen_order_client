82e0fcc5235bf1c101936fe22eb5c7e999dffd10
a635941266 <junbin.liu@mobilemd.cn>
2014-09-14 18:53:20 +0800

    Update 2014-09-14T18:53:20.817524

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash 
+git filter-branch --env-filter ' 
+timeStamp=`date +%s`
+# # 加3年半
+# let futureTime=$timeStamp+$[$RANDOM*10000]
+
+# 当前
+let futureTime=$timeStamp
+
+# 减4个月
+let startTime=$timeStamp-10000000   
+# 平均值
+let avg=(futureTime-startTime)/1000
+# 增加波动值
+let time=$startTime+$avg+$RANDOM
+let index=$index+1
+let countadd=$countadd+$avg+$RANDOM
+let c=$countadd+$startTime
+echo "********"
+
+CORRECT_NAME="PercyJon"
+CORRECT_EMAIL="PercyJon.H@gmail.com"
+
+if [ $GIT_COMMIT != 119f9ecf58069b265ab22f1f97d2b648faf932e0 ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+    # export GIT_AUTHOR_DATE="$c"
+    # export GIT_COMMITTER_DATE="$c"
+fi
+
+' --tag-name-filter cat -- --branches --tags
---
+++ greenbox.py
@@ -0,0 +1,29 @@
+import datetime
+import os
+from random import randint
+
+now = datetime.datetime.now()
+start = now.replace(year=2014)
+
+
+def change_day():
+    return datetime.timedelta(days=1, seconds=randint(0, 60), minutes=randint(0, 60), hours=randint(0, 24))
+
+
+def change_time():
+    return datetime.timedelta(seconds=randint(0, 60), minutes=randint(0, 60))
+
+
+commit_date = (start + change_day())
+times = randint(5, 30)
+
+while commit_date < now:
+
+    commit_date = commit_date + change_day()
+    for i in range(times):
+        f = open('data.txt', 'a+')
+        commit_date = commit_date + change_time()
+        f.writelines(commit_date.isoformat() + '\n')
+        f.close()
+        os.system('git add .')
+        os.system('git commit --date={time} -m "Update {time}"'.format(time=commit_date.isoformat()))
