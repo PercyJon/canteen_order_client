bdbe42876e855528a1c979eb53be2d8111967373
a635941266 <junbin.liu@mobilemd.cn>
2014-09-17 03:57:03 +0800

    Update 2014-09-17T03:57:03.902482

--- greenbox.py
+++ greenbox.py
@@ -7,7 +7,7 @@ start = now.replace(year=2014)
 
 
 def change_day():
-    return datetime.timedelta(days=1, seconds=randint(0, 60), minutes=randint(0, 60), hours=randint(0, 24))
+    return datetime.timedelta(days=2, seconds=randint(0, 60), minutes=randint(0, 60), hours=randint(0, 24))
 
 
 def change_time():
@@ -15,7 +15,7 @@ def change_time():
 
 
 commit_date = (start + change_day())
-times = randint(5, 30)
+times = randint(0, 5)
 
 while commit_date < now:
 
